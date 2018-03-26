f4b4e16151802c8791a7d072b30b7bfe368542fa
KevLoi <w.kevnloi@gmail.com>
2018-03-25 19:47:27 -0700

    home, about, projects, bad

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use((request, response, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${request.method} ${request.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err) {
+			console.log('Unable to append to server.log.');
+		}
+	});
+	next();
+});
+
+// app.use((request, response, next) => {
+// 	response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+// app.get('/', (request, response) => {
+// 	// response.send('<h1>Hello Express</h1>');
+// 	response.send({
+// 		name: 'Kevin',
+// 		likes: [
+// 			'Basketball', 
+// 			'Food', 
+// 			'Steak'
+// 		]
+// 	});
+// });
+
+app.get('/', (request, response) => {
+	response.render('home.hbs', {
+		pageTitle: 'Home page',
+		welcomeMessage: 'Welcome to my website',
+	});
+});
+
+app.get('/about', (request, response) => {
+	response.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
+});
+
+app.get('/projects', (request, response) => {
+	response.render('projects.hbs', {
+		pageTitle: 'Projects'
+	});
+});
+
+// /bad - send back json with errorMessage 
+app.get('/bad', (request, response) => {
+	response.send({
+		errorMessage: 'Unable to handle request'
+	});
+})
+
+// Listen binds app to port on machine
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
